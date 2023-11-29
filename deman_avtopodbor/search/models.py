5313a53d17684cee3b426c04c71915faf940cc63
Bgbisdbg <dimon123098@bk.ru>
2023-11-29 19:07:22 +0300

    created models search

--- deman_avtopodbor/search/models.py
+++ deman_avtopodbor/search/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+class CarBrand(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class CarModel(models.Model):
+    name = models.CharField(max_length=64)
+    year = models.DecimalField(max_digits=10, decimal_places=0)
+    price = models.DecimalField(max_digits=30, decimal_places=2)
+    brand = models.ForeignKey(to=CarBrand, on_delete=models.CASCADE)
+    link = models.URLField(max_length=200)
