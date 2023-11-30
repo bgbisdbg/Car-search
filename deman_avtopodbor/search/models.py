852a2430e8186114510290d1e26ad03e1c8db649
Bgbisdbg <dimon123098@bk.ru>
2023-11-30 19:22:43 +0300

    Creating displaying a list of cars

--- deman_avtopodbor/search/models.py
+++ deman_avtopodbor/search/models.py
@@ -4,10 +4,12 @@ from django.db import models
 class CarBrand(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class CarModel(models.Model):
     name = models.CharField(max_length=64)
     year = models.DecimalField(max_digits=10, decimal_places=0)
     price = models.DecimalField(max_digits=30, decimal_places=2)
     brand = models.ForeignKey(to=CarBrand, on_delete=models.CASCADE)
-    link = models.URLField(max_length=200)
+    link = models.URLField(max_length=200, blank=True)
--- deman_avtopodbor/search/views.py
+++ deman_avtopodbor/search/views.py
@@ -2,9 +2,49 @@ from django.shortcuts import render
 from common.views import TitleMixin
 from django.views.generic.list import ListView
 from search.models import CarModel
+from django.shortcuts import redirect
 
 
+# Класс представления для поиска автомобилей
 class SearchCarListView(TitleMixin, ListView):
+    # Модель, используемая для отображения данных
     model = CarModel
+    # Имя шаблона, который будет использоваться для отображения данных
     template_name = 'search/search.html'
+    # Заголовок страницы
     title = "Поиск"
+
+    # Метод для получения набора данных для отображения
+    def get_queryset(self, brand_id=None):
+        # Получение базового набора данных
+        queryset = super(SearchCarListView, self).get_queryset()
+        # Получение значений фильтров из GET параметров
+        brand = self.request.GET.get('brand')
+        model = self.request.GET.get('model')
+        year = self.request.GET.get('year')
+        price = self.request.GET.get('price')
+
+        # Применение фильтров к набору данных
+        if model:
+            queryset = queryset.filter(name__icontains=model)
+
+        if price and price.isdigit():
+            queryset = queryset.filter(price=price)
+
+        return queryset
+
+   # Метод для получения контекста данных для отображения
+    def get_context_data(self, **kwargs):
+        # Получение базового контекста данных
+        context = super().get_context_data(**kwargs)
+        # Добавление флага сброса фильтров в контекст данных
+        context['reset_filters'] = self.request.GET.get('reset_filters', False)
+        return context
+
+    # Метод для обработки GET запроса
+    def get(self, request, *args, **kwargs):
+        # Если в GET параметрах есть флаг сброса фильтров, то перенаправляем пользователя на страницу поиска без параметров
+        if 'reset_filters' in request.GET:
+            return redirect('/search/search/')
+        # Иначе вызываем базовый метод get
+        return super().get(request, *args, **kwargs)
